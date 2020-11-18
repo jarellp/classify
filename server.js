633aa260384e0a265149fb9ce8f0a09333f80c8e
Casey Brown <cbrow123@my.tnstate.edu>
2020-11-18 19:12:45 +0000

    Casey fixed the merge conlict

--- server.js
+++ server.js
@@ -40,7 +40,6 @@ app.set('view engine', 'pug')
 app.get('/', function(req, res) {
     res.render('login');
 });
-<<<<<<< HEAD
 app.get('/signup/', function(req, res) {
     res.render('registration');
 });
@@ -48,8 +47,5 @@ app.get('/login/', function(req, res) {
     res.render('survey');
 });
 app.get('/submit/', function(req, res) {
-=======
-app.get('/page/', function(req, res) {
->>>>>>> 93c2ac14e0e2e5247426c132fe55c7cf44709015
     res.render('page');
 });
--- static/login.js
+++ static/login.js
@@ -1,4 +1,4 @@
-console.log("Hello world");
+console.log("Hello World");
 
 function signup() {
     let url = "/signup/";
@@ -7,25 +7,15 @@ function signup() {
 }
 
 function login() {
-<<<<<<< HEAD
     let url = "/login/";
     console.log(url);
-=======
-    let url = "/success/";
->>>>>>> 93c2ac14e0e2e5247426c132fe55c7cf44709015
     window.location.replace(url);
 }
 
 function setup() {
-<<<<<<< HEAD
     $("#signup").click(signup);
     $("#login").click(login);
-=======
-    $("#signup").click(webpage.registration);
-    $("#login").click(webpage.submission);
     
-    
->>>>>>> 93c2ac14e0e2e5247426c132fe55c7cf44709015
 }
 
 $(document).ready(setup);
